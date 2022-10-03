7a44a5f762dc3e88bb083c150557ce5622aec6a8
Mossse <moses6334@gmail.com>
2022-10-03 13:26:15 +0300

    created app

---
+++ app.js
@@ -0,0 +1,17 @@
+import { render } from 'ejs'
+import express from 'express'
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
+
+app.get('/', (rer, res) => {
+    render('/index')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`app is running on PORT ${PORT}`)
+})
