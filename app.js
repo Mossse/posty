c7e4ebb848653d5b8926d1a39644e8ad4daa3c4b
Mossse <moses6334@gmail.com>
2022-10-03 14:31:44 +0300

    configured db

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
-import { render } from 'ejs'
 import express from 'express'
+import mysql from 'mysql'
 
 const app = express()
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database:'posty'
+})
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
 
-app.get('/', (rer, res) => {
-    render('/index')
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// login: display form
+app.get('/login', (req, res) =>{
+    res.render('login')
+})
+
+// login: submit form
+app.post('/login', (req, res) => {
+    
+})
+
+
+// submit: display form
+app.get('/signup', (req, res) =>{
+    res.render('signup')
+})
+
+// submit: submit form
+app.post('/signup', (req, res) => {
+    
 })
 
 const PORT = process.env.PORT || 3000
